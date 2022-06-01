0f53af76f4d8d2dbb053d712c5be709fd108a333
poncho29 <meneses0828@gmail.com>
2022-06-01 08:22:02 -0500

    agregando funcionalidad del boton

--- src/componets/pure/Contact.js
+++ src/componets/pure/Contact.js
@@ -1,14 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 import { ContactClass } from '../../models/contact.class';
 
 const Contact = ({ contact }) => {
+  const [connected, setConnected] = useState(contact.connected);
+
   return (
     <>
       <h1>Nombre: { contact.name }</h1>
       <h2>Nombre: { contact.lastname }</h2>
       <h3>Email: { contact.email }</h3>
-      <h4>Conectado: { (contact.connected) ? 'Connected' : 'Disconnected' }</h4>
+      <h4>Status: { (connected) ? 'Connected' : 'Disconnected' }</h4>
+      <button onClick={() => setConnected(!connected)}>{(connected) ? 'Connected' : 'Disconnected'}</button>
     </>
   )
 }
